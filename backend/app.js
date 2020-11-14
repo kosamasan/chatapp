e6ce85167b5ab2b4f8e5d96e1aad6b984fccc149
Antonis Kosm <kosmasantonios@yahoo.gr>
2020-11-15 00:14:46 +0200

    Create place controller, 404 routes handling.

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const placesRoutes = require('./routes/places-routes');
+const HttpError = require('./models/http-error');
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.use('/api/places', placesRoutes);
+
+app.use((req, res, next) => {
+    const error = new HttpError('Could not find this url.', 404);
+    throw error;
+});
 
 // express sees that the function has 4 parameters and handles it as an error middleware
 app.use((error, req, res, next) => {
