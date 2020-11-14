202535f534d3eba4232a054bdb5fc3bbdcae2a73
Antonis Kosm <kosmasantonios@yahoo.gr>
2020-11-14 21:04:43 +0200

    Initialize BE and FE

---
+++ backend/routes/places-routes.js
@@ -0,0 +1,50 @@
+const express = require('express');
+
+const router = express.Router();
+
+const DUMMY_PLACES = [
+	{
+		id: 'p1',
+		title: 'Empire State Building',
+		description: 'famous building',
+		location: {
+			lat: 40.7484474,
+			lng: -73.9871516,
+		},
+		address: 'New York, NY 10001',
+		creator: 'u1',
+	},
+];
+
+router.get('/:pid', (req, res, next) => {
+	const placeId = req.params.pid;
+	const place = DUMMY_PLACES.find((p) => {
+		return p.id === placeId;
+	});
+
+	if (!place) {
+        const error = new Error('Could nod find a place for the provided id.');
+        error.code = 404;
+        throw error;
+	}
+
+	res.json({ place: place });
+});
+
+router.get('/user/:uid', (req, res, next) => {
+	const userId = req.params.uid;
+
+	const place = DUMMY_PLACES.find((p) => {
+		return p.creator === userId;
+    });
+    
+    if (!place) {
+        const error = new Error('Could nod find a place for the provided user id.');
+        error.code = 404;
+        return next(error)
+	}
+
+	res.json({ place });
+});
+
+module.exports = router;
