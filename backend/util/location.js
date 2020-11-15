2bac9f9312d9b0b01079f77c89089394f06ee0d3
Antonis Kosm <kosmasantonios@yahoo.gr>
2020-11-16 01:08:51 +0200

    coordinates utility

---
+++ backend/util/location.js
@@ -0,0 +1,6 @@
+// it should be replaced with google api
+async function getCoordsForAddress(address) {
+    return { lat: 40.7484474, lng: -73.9871516 }
+};
+
+module.exports = getCoordsForAddress;
