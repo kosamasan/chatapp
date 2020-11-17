f4c724f83106cc1218692c88c321223e9f63bf4f
Antonis Kosm <kosmasantonios@yahoo.gr>
2020-11-17 16:43:19 +0200

    Database setup, places schema

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/users-routes');
@@ -13,17 +14,24 @@ app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
 
 app.use((req, res, next) => {
-    const error = new HttpError('Could not find this url.', 404);
-    throw error;
+	const error = new HttpError('Could not find this url.', 404);
+	throw error;
 });
 
 // express sees that the function has 4 parameters and handles it as an error middleware
 app.use((error, req, res, next) => {
-    if (res.headerSent) {
-        return next(error);
-    }
-    res.status(error.code || 500)
-    res.json({message: error.message || 'An unknown error occurred!'});
+	if (res.headerSent) {
+		return next(error);
+	}
+	res.status(error.code || 500);
+	res.json({ message: error.message || 'An unknown error occurred!' });
 });
 
-app.listen(5000);
+mongoose
+.connect("mongodb+srv://anto:5BHZvC9KbbsNI4qV@cluster0.dqylc.mongodb.net/chatapp?retryWrites=true", { useCreateIndex: true, useNewUrlParser: true })
+.then(() => {
+    app.listen(5000);
+})
+.catch(err => {
+    console.log(err);
+});
