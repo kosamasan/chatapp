0da2b74580915f4ca69eb6be48da58f65d5993f8
Antonis Kosm <kosmasantonios@yahoo.gr>
2020-11-19 16:35:22 +0200

    Login, signup and getusers

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -1,56 +1,76 @@
-const {v4 : uuidv4} = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 const { validationResult } = require('express-validator');
 
 const HttpError = require('../models/http-error');
+const User = require('../models/user');
 
-const DUMMY_USERS = [
-    {
-        id: 'u1',
-        name: 'Kosm Ant',
-        email: 'test@test.com',
-        password: 'test'
+const getUsers = async (req, res, next) => {
+    let users;
+    try {
+        users = await User.find({}, '-password');
+    } catch (err) {
+		const error = new HttpError('Something went wrong', 500);
+		return next(error);
     }
-]
-
-const getUsers = (req, res, next) => {
-    res.json({users: DUMMY_USERS});
+    
+    res.json({users: users.map(user => user.toObject({ getters: true }))});
 };
 
-const signup = (req, res, next) => {
-    const errors = validationResult(req);
+const signup = async (req, res, next) => {
+	const errors = validationResult(req);
 	if (!errors.isEmpty()) {
-		throw new HttpError('Invalid input', 422)
-    }
-    
-    const { name, email, password } = req.body;
+		return next(HttpError('Invalid input', 422));
+	}
 
-    const hasUser = DUMMY_USERS.find(u => u.email === email);
-    if (hasUser) {
-        throw new HttpError('email already exists', 422);
-    }
+	const { name, email, password, places } = req.body;
 
-    const createdUser = {
-        id: uuidv4(),
-        name,
-        email,
-        password
-    };
+	let existingUser;
+	try {
+		existingUser = await User.findOne({ email: email });
+	} catch (err) {
+		const error = new HttpError('Something went wrong', 500);
+		return next(error);
+	}
 
-    DUMMY_USERS.push(createdUser);
+	if (existingUser) {
+		const error = new HttpError('User already exists', 422);
+		return next(error);
+	}
 
-    res.status(201).json({user: createdUser});
+	const createdUser = new User({
+		name,
+		email,
+		image: 'https://static.thenounproject.com/png/17241-200.png',
+		password,
+		places,
+	});
+
+	try {
+		await createdUser.save();
+	} catch (err) {
+		const error = new HttpError('Something went wrong', 500);
+		return next(error);
+	}
+
+	res.status(201).json({ user: createdUser.toObject({ getters: true }) });
 };
 
-const login = (req, res, next) => {
-    const { email, password } = req.body;
+const login = async (req, res, next) => {
+	const { email, password } = req.body;
 
-    const identifiedUser = DUMMY_USERS.find(u => u.email === email);
+	let existingUser;
+	try {
+		existingUser = await User.findOne({ email: email });
+	} catch (err) {
+		const error = new HttpError('Something went wrong', 500);
+		return next(error);
+	}
 
-    if (!identifiedUser || identifiedUser.password !== password) {
-        throw new HttpError('User not found.', 401);
-    }
-    
-    res.json({message: 'logged in'});
+	if (!existingUser || existingUser.password !== password) {
+		return next(new HttpError('User not found.', 401));
+	}
+
+	res.json({ message: 'logged in' });
 };
 
 exports.getUsers = getUsers;
