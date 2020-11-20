148f1b439cb88376da1c98cb01927c10f48788fa
Antonis Kosm <kosmasantonios@yahoo.gr>
2020-11-20 17:32:20 +0200

    Handle cors

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,13 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+	next();
+});
+
 app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
 
@@ -28,10 +35,13 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-.connect("mongodb+srv://anto:5BHZvC9KbbsNI4qV@cluster0.dqylc.mongodb.net/chatapp?retryWrites=true", { useCreateIndex: true, useNewUrlParser: true })
-.then(() => {
-    app.listen(5000);
-})
-.catch(err => {
-    console.log(err);
-});
+	.connect('mongodb+srv://anto:5BHZvC9KbbsNI4qV@cluster0.dqylc.mongodb.net/chatapp?retryWrites=true', {
+		useCreateIndex: true,
+		useNewUrlParser: true,
+	})
+	.then(() => {
+		app.listen(5000);
+	})
+	.catch((err) => {
+		console.log(err);
+	});
